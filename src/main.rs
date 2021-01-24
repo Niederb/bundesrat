5d3a1f8a20db36cf028d9e3afe705ff1557c6c52
Thomas Niederberger <thomas@niederb.ch>
2021-01-24 10:09:12 +0100

    Sort groups

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use polars::prelude::*;
 use std::fs::File;
 
-fn load_data() -> Result<DataFrame> {
+fn load_data() -> DataFrame {
     let file = File::open("bundesrat.csv").expect("could not open file");
   
-    CsvReader::new(file)
+    let elected = Field::new("Gewählt", DataType::Utf8);
+    let stepped_down = Field::new("Zurückgetreten", DataType::Utf8);
+    
+    let schema = Schema::new(vec![elected, stepped_down]);
+    let df = CsvReader::new(file)
             .infer_schema(None)
             .has_header(true)
-            //.with_one_thread(true) // set this to false to try multi-threaded parsing
             .with_delimiter(b';')
             .with_ignore_parser_errors(true)
-            .finish()
+            .with_dtype_overwrite(Some(&schema))
+            .finish().unwrap();
+    //let fmt = "%d.%m.%Y";//%Y-%m-%d";
+    //let s0 = Date32Chunked::parse_from_str_slice("Gewählt", df.column("Gewählt").unwrap().utf8().unwrap().cont_slice(), fmt).into();
+    df
   }
 
 fn main() {
-    let df = load_data().unwrap();
+    let df = load_data();
     let df_shape = df.shape();
     println!("Dataframe rows/cols: {}/{}", df_shape.0, df_shape.1);
-    println!("{:?}", df.select("Name"));
+    println!("{:?}", df);
 
     let gender = {
         (&df).groupby("Geschlecht").unwrap().select("Name").count()
@@ -25,15 +32,15 @@ fn main() {
     println!("{:?}", gender);
 
     let party = {
-        (&df).groupby("Partei").unwrap().select("Name").count()
+        (&df).groupby("Partei").unwrap().select("Partei").count()
     }.unwrap();
+    let party = party.sort("Partei_count", true);
     println!("{:?}", party);
 
-    let mut kanton = {
-        (&df).groupby("Kanton").unwrap().select("Name").count()
+    let kanton = {
+        (&df).groupby("Kanton").unwrap().select("Kanton").count()
     }.unwrap();
-    for k in kanton.iter_record_batches(10) {
-        println!("{:?}", k);
-    }
+    let kanton = kanton.sort("Kanton_count", true);
+    println!("{:?}", kanton);
     
 }
