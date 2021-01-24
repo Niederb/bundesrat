5c4da92ab9fa47f5d995b2d50ae9304635ec6707
Thomas Niederberger <thomas@niederb.ch>
2021-01-24 11:19:32 +0100

    Create a plot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use polars::prelude::*;
 use std::fs::File;
 
+use plotly::common::{
+    ColorScale, ColorScalePalette, DashType, Fill, Font, Line, LineShape, Marker, Mode, Title,
+};
+use plotly::layout::{Axis, BarMode, Layout, Legend, TicksDirection};
+use plotly::{Bar, NamedColor, Plot, Rgb, Rgba, Scatter};
+
 fn load_data() -> DataFrame {
     let file = File::open("bundesrat.csv").expect("could not open file");
   
@@ -19,6 +25,24 @@ fn load_data() -> DataFrame {
     //let s0 = Date32Chunked::parse_from_str_slice("Gewählt", df.column("Gewählt").unwrap().utf8().unwrap().cont_slice(), fmt).into();
     df
   }
+
+fn create_plot(kanton: &DataFrame) {
+    let labels_series = kanton.column("Kanton").unwrap();
+    let labels: Vec<String> = labels_series.utf8().unwrap().into_iter().map(Option::unwrap).map(|s| s.to_string()).collect();
+    let values_series = kanton.column("Kanton_count").unwrap();
+    let values = values_series.u32().unwrap().into_iter().map(Option::unwrap).collect();
+
+    //values = values.collect().to_owned();
+    let trace: Box<Bar<String, u32>> = Bar::new(labels, values);
+    // https://www.snb.ch/de/iabout/pub/id/pub_annrep
+    let layout = Layout::new().title(Title::new("Swiss National Bank Annual Report Length"))
+        .x_axis(Axis::new().title(Title::new("Kanton")))
+        .y_axis(Axis::new().title(Title::new("Anzahl Bundesräte")));
+    let mut plot = Plot::new();
+    plot.set_layout(layout);
+    plot.add_trace(trace);
+    plot.show();
+}
 
 fn main() {
     let df = load_data();
@@ -40,7 +64,8 @@ fn main() {
     let kanton = {
         (&df).groupby("Kanton").unwrap().select("Kanton").count()
     }.unwrap();
-    let kanton = kanton.sort("Kanton_count", true);
-    println!("{:?}", kanton);
+    let kanton = kanton.sort("Kanton_count", true).unwrap();
+    println!("{:?}", &kanton);
+    create_plot(&kanton);
     
 }
