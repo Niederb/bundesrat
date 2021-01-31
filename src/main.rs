9f71271892cb442d23e0c9ced9be2970fc07b5e4
Thomas Niederberger <thomas@niederb.ch>
2021-01-31 17:24:33 +0100

    Convert date column and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use polars::prelude::*;
 use std::fs::File;
 
-use plotly::common::{
-    ColorScale, ColorScalePalette, DashType, Fill, Font, Line, LineShape, Marker, Mode, Title,
-};
-use plotly::layout::{Axis, BarMode, Layout, Legend, TicksDirection};
-use plotly::{Bar, NamedColor, Plot, Rgb, Rgba, Scatter};
+use plotly::common::Title;
+use plotly::layout::{Axis, Layout};
+use plotly::{Bar, Plot};
+
+fn to_date_column(s: &Series) -> ChunkedArray<Date32Type> {
+    let elected: Vec<&str> = s.utf8().unwrap().into_iter().map(|s| s.unwrap()).collect();
+    let fmt = "%d.%m.%Y";
+    let s: ChunkedArray<Date32Type> =
+        Date32Chunked::parse_from_str_slice("Gewählt", &elected, fmt);
+    s
+}
 
 fn load_data() -> DataFrame {
     let file = File::open("bundesrat.csv").expect("could not open file");
@@ -24,18 +30,12 @@ fn load_data() -> DataFrame {
         .unwrap();
 
     let elected_series = df.column("Gewählt").unwrap();
-    let elected: Vec<&str> = elected_series
-        .utf8()
-        .unwrap()
-        .into_iter()
-        .map(|s| s.unwrap())
-        .collect();
-    let fmt = "%d.%m.%Y"; //%Y-%m-%d";
-    let s0: ChunkedArray<Date32Type> =
-        Date32Chunked::parse_from_str_slice("Gewählt", &elected, fmt).into();
-    println!("Date: {:?}", s0);
-    //df.add_column(s0).unwrap();
-    df.replace("Gewählt", s0);
+    let elected_series = to_date_column(elected_series);
+    df.replace("Gewählt", elected_series).unwrap();
+
+    let elected_series = df.column("Zurückgetreten").unwrap();
+    let elected_series = to_date_column(elected_series);
+    df.replace("Zurückgetreten", elected_series).unwrap();
     df
 }
 
@@ -55,9 +55,8 @@ fn create_plot(kanton: &DataFrame) {
         .map(Option::unwrap)
         .collect();
 
-    //values = values.collect().to_owned();
     let trace: Box<Bar<String, u32>> = Bar::new(labels, values);
-    // https://www.snb.ch/de/iabout/pub/id/pub_annrep
+
     let layout = Layout::new()
         .title(Title::new("Swiss National Bank Annual Report Length"))
         .x_axis(Axis::new().title(Title::new("Kanton")))
