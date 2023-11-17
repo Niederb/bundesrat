b02c3a92c44cbbb77c831d1259042e6924f552ba
Thomas Niederberger <thomas@niederb.ch>
2023-11-17 10:44:47 +0100

    Expand analysis

--- polars_test.py
+++ polars_test.py
@@ -1,14 +1,25 @@
 import polars as pl
 
-s = pl.Series("a", [1, 2, 3, 4, 5])
-print(s)
+data = pl.read_csv("bundesrat.csv", separator=";", try_parse_dates=True)
 
-data = pl.read_csv("bundesrat.csv", separator=";")
+data = data.with_columns(pl.col("Elected").str.to_date("%d.%m.%Y"))
+data = data.with_columns(pl.col("Elected").dt.month().alias("MonthElected"))
+data = data.with_columns(pl.col("Retired").str.to_date("%d.%m.%Y"))
 
+data = data.with_columns((pl.col("Retired") - pl.col("Elected")).alias("Term"))
+
+# Make sure that exactly seven members are currently active (have not resigned)
+assert data.filter(pl.col("Term").is_null()).height == 7
+print(data.filter(pl.col("Term").is_not_null()).sort("Term", descending=True))
 print(f"Total members in the council: {data.height}")
+print(data.select(pl.col("Term")).describe())
 
-group_by_party = data.group_by(by="Party")
-print(group_by_party.count())
+print(data.group_by(by="Party").count().sort("count", descending=True))
 
-group_by_sex = data.group_by(by="Sex")
-print(group_by_sex.count())
+print(data.group_by(by="Sex").count().sort("count", descending=True))
+
+by_canton = data.group_by(by="Kanton").count().sort("count", descending=True)
+print(by_canton)
+print(f"Cantons without federal council: {26 - by_canton.height}")
+
+print(data.group_by(by="MonthElected").count().sort("count", descending=True))
